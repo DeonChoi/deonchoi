fcf65e0b6cbc66af1504345eadbaf2ca47f20d88
Deon Choi <deon.choi@gmail.com>
2019-12-25 17:24:04 -0800

    media queries

--- src/components/Work.js
+++ src/components/Work.js
@@ -15,16 +15,21 @@ const Work = () => {
         <div className='d-flex flex-wrap align-items-center align-content-center justify-content-center text-center text-monospace text-white workContainer'>
            <Zoom>
             <div className='projectCard'>
-                <a target='_blank' href='https://shrinkurlapp.herokuapp.com/api/' rel='noopener noreferrer' className=''>
-                    <img src={ShrinkifyIcon} alt='Shrinkify Project' className='projectImage'/>
-                </a>
-                <div className='align-items-center justify-content-center d-flex'>
-                    <a target='_blank' href='https://github.com/DeonChoi/shrinkURL' rel='noopener noreferrer' className=''>
-                        <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
-                    </a>
-                    <span className='projectTitle'>Shrinkify</span>
-                </div>
                 
+                <div className='projectInfo'>
+                    <a target='_blank' href='https://shrinkurlapp.herokuapp.com/api/' rel='noopener noreferrer' className=''>
+                        <img src={ShrinkifyIcon} alt='Shrinkify Project' className='projectImage'/>
+                    </a>
+
+                    <div className='align-items-center justify-content-center d-flex'>
+                        <a target='_blank' href='https://github.com/DeonChoi/shrinkURL' rel='noopener noreferrer' className=''>
+                            <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+                        </a>
+                        <span className='projectTitle'>Shrinkify</span>
+                    </div>
+
+                </div>
+
                 <div className='techstackRow'>
                     <span className='badge badge-dark techstack'>Node.js</span>
                     <span className='badge badge-dark techstack'>Express.js</span>
@@ -32,20 +37,23 @@ const Work = () => {
                     <span className='badge badge-dark techstack'>MongoDB</span>
                     <span className='badge badge-dark techstack'>Bootstrap</span>
                 </div>
-                
+
             </div>
 
             <div className='projectCard'>
-                <a target='_blank' href='https://deonchoi.github.io/flix-app/' rel='noopener noreferrer' className=''>
-                    <img src={FlixIcon} alt='Shrinkify Project' className='projectImage'/>
-                </a>
-                <div className='align-items-center justify-content-center d-flex'>
-                    <a target='_blank' href='https://github.com/DeonChoi/flix-app' rel='noopener noreferrer' className=''>
-                        <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+
+                <div className='projectInfo'>
+                    <a target='_blank' href='https://deonchoi.github.io/flix-app/' rel='noopener noreferrer' className=''>
+                        <img src={FlixIcon} alt='Shrinkify Project' className='projectImage'/>
                     </a>
-                    <span className='projectTitle'>Flix</span>
+                    <div className='align-items-center justify-content-center d-flex'>
+                        <a target='_blank' href='https://github.com/DeonChoi/flix-app' rel='noopener noreferrer' className=''>
+                            <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+                        </a>
+                        <span className='projectTitle'>Flix</span>
+                    </div>
                 </div>
-                
+
                 <div className='techstackRow'>
                     <span className='badge badge-dark techstack'>React.js</span>
                     <span className='badge badge-dark techstack'>Bootstrap</span>
@@ -55,16 +63,19 @@ const Work = () => {
             </div>
 
             <div className='projectCard'>
-                <a target='_blank' href='https://deonchoi.github.io/recipe-app/' rel='noopener noreferrer' className=''>
-                    <img src={DelishIcon} alt='Shrinkify Project' className='projectImage'/>
-                </a>
-                <div className='align-items-center justify-content-center d-flex'>
-                    <a target='_blank' href='https://github.com/DeonChoi/recipe-app' rel='noopener noreferrer' className=''>
-                        <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+
+                <div className='projectInfo'>
+                    <a target='_blank' href='https://deonchoi.github.io/recipe-app/' rel='noopener noreferrer' className=''>
+                        <img src={DelishIcon} alt='Shrinkify Project' className='projectImage'/>
                     </a>
-                    <span className='projectTitle'>Delish</span>
+                    <div className='align-items-center justify-content-center d-flex'>
+                        <a target='_blank' href='https://github.com/DeonChoi/recipe-app' rel='noopener noreferrer' className=''>
+                            <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+                        </a>
+                        <span className='projectTitle'>Delish</span>
+                    </div>
                 </div>
-                
+
                 <div className='techstackRow'>
                 <span className='badge badge-dark techstack'>React.js</span>
                     <span className='badge badge-dark techstack'>Bootstrap</span>
@@ -74,15 +85,17 @@ const Work = () => {
             </div>
 
             <div className='projectCard'>
-                <a target='_blank' href='https://deonchoi.github.io/OOPgameShowApp/' rel='noopener noreferrer' className=''>
-                    <img src={PhraseHunterIcon} alt='Shrinkify Project' className='projectImage'/>
-                </a>
-                <div className='align-items-center justify-content-center d-flex'>
-                    <a target='_blank' href='https://github.com/DeonChoi/OOPgameShowApp' rel='noopener noreferrer' className=''>
-                        <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+                <div className='projectInfo'>
+                    <a target='_blank' href='https://deonchoi.github.io/OOPgameShowApp/' rel='noopener noreferrer' className=''>
+                        <img src={PhraseHunterIcon} alt='Shrinkify Project' className='projectImage'/>
                     </a>
-                    <span className='projectTitle'>Phrase Hunter</span>
-                </div>
+                    <div className='align-items-center justify-content-center d-flex'>
+                        <a target='_blank' href='https://github.com/DeonChoi/OOPgameShowApp' rel='noopener noreferrer' className=''>
+                            <img src={GitHubIon} alt='Github Repository' className='githubIcon'/>
+                        </a>
+                        <span className='projectTitle'>Phrase Hunter</span>
+                    </div>
+                </div>    
                 
                 <div className='techstackRow'>
                     <span className='badge badge-dark techstack'>HTML</span>
