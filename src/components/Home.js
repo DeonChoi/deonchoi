01d902f925453f5f00b22e4478e15ab72c15847d
Deon Choi <deon.choi@gmail.com>
2019-12-29 18:19:54 -0800

    semantic markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,45 +8,53 @@ const Home = () => {
     return (
         <main className='d-flex flex-column text-monospace align-items-center justify-content-center homeContainer'>
         <Zoom>
-            <Scramble autoStart text='Deon Choi' className='display-2 homeName'
-                steps={[
-                {
-                    roll: 25,
-                    action: '+',
-                    type: 'all',
-                },
-                {
-                    action: '-',
-                    type: 'forward',
-                },
-                ]}
-            />
-            <Scramble autoStart text='Full Stack Developer' className='display-4 homeDescription'
-                steps={[
-                {
+            <h1>
+                <Scramble autoStart text='Deon Choi' className='display-2 homeName'
+                    steps={[
+                    {
+                        roll: 25,
+                        action: '+',
+                        type: 'all',
+                    },
+                    {
+                        action: '-',
+                        type: 'forward',
+                    },
+                    ]}
+                />
+            </h1>
+            
+            <h2>
+                <Scramble autoStart text='Full Stack Developer' className='display-4 homeDescription'
+                    steps={[
+                    {
+                        roll: 20,
+                        action: '+',
+                        type: 'all',
+                    },
+                    {
+                        action: '-',
+                        type: 'forward',
+                    },
+                    ]}
+                />
+            </h2>
+            
+            <h2>
+                <Scramble autoStart text='San Francisco Bay Area' className='display-4 homeDescription'
+                    steps={[
+                    {
                     roll: 20,
-                    action: '+',
-                    type: 'all',
-                },
-                {
-                    action: '-',
-                    type: 'forward',
-                },
-                ]}
-            />
-            <Scramble autoStart text='San Francisco Bay Area' className='display-4 homeDescription'
-                steps={[
-                {
-                roll: 20,
-                    action: '+',
-                    type: 'all',
-                },
-                {
-                    action: '-',
-                    type: 'forward',
-                },
-                ]}
-            />
+                        action: '+',
+                        type: 'all',
+                    },
+                    {
+                        action: '-',
+                        type: 'forward',
+                    },
+                    ]}
+                />
+            </h2>
             </Zoom>
         </main>
     );
